22a0ea13c974ef0980bc15c953bde8d956cf0802
rubenossorio25-max <rubenossorio25@gmail.com>
2025-12-09 13:19:52 +0000

    depurado

--- include/WebServer.h
+++
@@ -1,8 +0,0 @@
-#ifndef WEB_SERVER_H
-#define WEB_SERVER_H
-
-// Este archivo ha sido reemplazado por LuminaWebServer.h para ESP32
-// Importar el nuevo header para compatibilidad
-#include "LuminaWebServer.h"
-
-#endif // WEB_SERVER_H
